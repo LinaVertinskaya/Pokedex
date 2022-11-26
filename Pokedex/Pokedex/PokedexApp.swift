c168c5eb9e60067e61dd6509c783352e4eaa0e20
LinaVertinskaya <linavertinskayaa@gmail.com>
2022-11-26 21:31:56 +0300

    Add my project

---
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Лина Вертинская on 24.11.22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PokedexView()
+        }
+    }
+}
